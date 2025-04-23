a42d40032e65a40a72b5262a639847f79a67bc68
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-23 19:04:59 -0300

    feat: add UpdateUserUseCase

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -6,7 +6,6 @@ import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
-        // verificar se o email ja esta em uso
         const getUserByEmailRepository = new PostgresGetUserEmailRepository()
 
         const userWithProvidedEmail = await getUserByEmailRepository.execute(
@@ -17,20 +16,15 @@ export class CreateUserUseCase {
             throw new EmailAlreadyInUseError(createUserParams.email)
         }
 
-        // gerar ID do usuário
         const userId = uuidv4()
 
-        // criptografar a senha
         const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
 
-        // inserir o usuario no banco de dados
         const user = {
             ...createUserParams,
             id: userId,
             password: hashedPassword,
         }
-
-        // chamar o repositorio
 
         const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
---
+++ src/use-cases/update-user.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt'
+import { PostgresGetUserEmailRepository } from '../repositories/postgres/get-user-email.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        // se o email tiver sendo atualizado, verifica-lo se ja esta em uso
+
+        if (updateUserParams.email) {
+            const getUserByEmailRepository =
+                new PostgresGetUserEmailRepository()
+
+            const userWithProvidedEmail =
+                await getUserByEmailRepository.execute(updateUserParams.email)
+
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+
+        const updateUserRepository = new PostgresUpdateUserRepository()
+        const updateUser = await updateUserRepository.execute(userId, user)
+
+        return updateUser
+    }
+}
