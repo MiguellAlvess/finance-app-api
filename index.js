ba068ee92838decfebfed557e15b3f6c97c2cc73
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-25 19:28:25 -0300

    fix: fix CreateTransactionController required fields validation

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import {
     makeUpdateUserController,
 } from './src/factories/controllers/user.js'
 
+import { makeCreateTransactionController } from './src/factories/controllers/transaction.js'
+
 const app = express()
 
 app.use(express.json())
@@ -40,6 +42,14 @@ app.delete('/api/users/:userId', async (req, res) => {
     const deleteUserController = makeDeleteUserController()
 
     const { statusCode, body } = await deleteUserController.execute(req)
+
+    res.status(statusCode).send(body)
+})
+
+app.post('/api/transactions', async (req, res) => {
+    const createTransactionController = makeCreateTransactionController()
+
+    const { statusCode, body } = await createTransactionController.execute(req)
 
     res.status(statusCode).send(body)
 })
