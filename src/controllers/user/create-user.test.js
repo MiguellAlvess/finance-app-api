5e2bfaf86ee2fde8fef42b1c2f2d389c7dd45bca
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-28 20:52:39 -0300

    test: ensure 201 is returned when user is created

---
+++ src/controllers/user/create-user.test.js
@@ -0,0 +1,30 @@
+import { CreateUserController } from './create-user.js'
+describe('Create User Controller', () => {
+    class CreateUserUseCaseStuby {
+        execute(user) {
+            return user
+        }
+    }
+    it('should returns 201 when creating a user successfully', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStuby()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Miguel',
+                last_name: 'Alves',
+                email: 'miguel2@gmail.com',
+                password: '1234567',
+            },
+        }
+
+        // act
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(201)
+        expect(result.body).toBe(httpRequest.body)
+    })
+})
