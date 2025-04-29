f6520da0aeaf1d0a4939b3755d6c158b12432c19
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-28 21:13:32 -0300

    test: ensure 400 is retunerd if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -27,4 +27,26 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
     })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStuby()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                last_name: 'Alves',
+                email: 'miguel2@gmail.com',
+                password: '1234567',
+            },
+        }
+
+        // act
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+
+        expect(result.statusCode).toBe(400)
+    })
 })
