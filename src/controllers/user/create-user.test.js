9d5497a7156d79f77c8764d55f836038a4dd08a2
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-30 17:31:49 -0300

    test: ensure CreateUserController returns 500 if CreateUserUseCase throws

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -201,4 +201,33 @@ describe('Create User Controller', () => {
 
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
     })
+
+    it('should return 500 if CreateUserUseCase throws', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStuby()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 6,
+                }),
+            },
+        }
+
+        jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        // act
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+
+        expect(result.statusCode).toBe(500)
+    })
 })
