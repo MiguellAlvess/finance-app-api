f33c6547553dd277eae89e05a0eabefc0d304648
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-02 18:38:12 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,9 +1,4 @@
-import { badRequest, notFound } from './http.js'
-
-export const emailIsAlreadyInUseResponse = () =>
-    badRequest({
-        message: 'Invalid e-mail. Please provide a valid one',
-    })
+import { notFound } from './http.js'
 
 export const userNotFoundResponse = () =>
     notFound({
