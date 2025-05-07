5dcda79ae5cf1aec8012bb1a592fda6043595228
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-07 17:35:13 -0300

    test: ensure PostgresGetUserBalanceRepository throws if prisma throws

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -5,6 +5,7 @@ import { PostgresGetUserBalanceRepository } from './get-user-balance.js'
 
 describe('Get User Balance Repository', () => {
     it('should get user balance on db', async () => {
+        // assert
         const user = await prisma.user.create({
             data: fakeUser,
         })
@@ -34,11 +35,12 @@ describe('Get User Balance Repository', () => {
                 },
             ],
         })
-
         const sut = new PostgresGetUserBalanceRepository()
 
+        // act
         const result = await sut.execute(user.id)
 
+        // assert
         expect(result.earnings.toString()).toBe('10000')
         expect(result.expenses.toString()).toBe('2000')
         expect(result.investments.toString()).toBe('6000')
@@ -46,11 +48,14 @@ describe('Get User Balance Repository', () => {
     })
 
     it('should call Prisma with correct params', async () => {
+        // arrange
         const sut = new PostgresGetUserBalanceRepository()
         const prismaSpy = jest.spyOn(prisma.transaction, 'aggregate')
 
+        // act
         await sut.execute(fakeUser.id)
 
+        // assert
         expect(prismaSpy).toHaveBeenCalledTimes(3)
         expect(prismaSpy).toHaveBeenCalledWith({
             where: {
@@ -79,5 +84,19 @@ describe('Get User Balance Repository', () => {
                 amount: true,
             },
         })
+    })
+
+    it('should throw if prisma throws', async () => {
+        // arrange
+        const sut = new PostgresGetUserBalanceRepository()
+        jest.spyOn(prisma.transaction, 'aggregate').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const promise = sut.execute(fakeUser.id)
+
+        // assert
+        await expect(promise).rejects.toThrow()
     })
 })
