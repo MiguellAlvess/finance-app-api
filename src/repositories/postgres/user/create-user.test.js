e3eeb39e5d27ae79ec3fc396b112467ce3a0de61
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-07 17:29:19 -0300

    test: ensure PostgresCreateUserRepository throws if prisma throws

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -4,10 +4,13 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 describe('Create User Repository', () => {
     it('should create a user on db', async () => {
+        // arrange
         const sut = new PostgresCreateUserRepository()
 
+        // act
         const result = await sut.execute(user)
 
+        // assert
         expect(result.id).toBe(user.id)
         expect(result.first_name).toBe(user.first_name)
         expect(result.last_name).toBe(user.last_name)
@@ -16,13 +19,28 @@ describe('Create User Repository', () => {
     })
 
     it('should call prisma with correct params', async () => {
+        // arrange
         const sut = new PostgresCreateUserRepository()
         const prismaSpy = jest.spyOn(prisma.user, 'create')
 
+        // act
         await sut.execute(user)
 
+        // assert
         expect(prismaSpy).toHaveBeenCalledWith({
             data: user,
         })
+    })
+
+    it('should throw if prisma throws', async () => {
+        // arrange
+        const sut = new PostgresCreateUserRepository()
+        jest.spyOn(prisma.user, 'create').mockRejectedValueOnce(new Error())
+
+        // act
+        const promise = sut.execute(user)
+
+        // assert
+        await expect(promise).rejects.toThrow()
     })
 })
