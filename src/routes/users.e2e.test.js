7e5d1bc2ebd26e78ede490753d9a96340999aa4c
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-08 19:52:14 -0300

    test: ensure GET /api/users returns 200 when user is found

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,5 +1,5 @@
 import supertest from 'supertest'
-import { app } from '../..'
+import { app } from '../app.js'
 import { user } from '../tests/index.js'
 
 describe('User Routes E2E Tests', () => {
@@ -12,5 +12,21 @@ describe('User Routes E2E Tests', () => {
             })
 
         expect(response.status).toBe(201)
+    })
+
+    it('GET /api/users should return 200 when user is found', async () => {
+        const { body: createdUser } = await supertest(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const response = await supertest(app).get(
+            `/api/users/${createdUser.id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(createdUser)
     })
 })
