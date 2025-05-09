682c4ac26c12805db30fab2dbf033f733c895c49
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-09 16:29:04 -0300

    test: ensure GET /api/transactions?userId returns 200 when fatching transactions successfully

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -25,4 +25,28 @@ describe('Transactions Routes E2E Tests', () => {
         expect(response.body.type).toBe(transaction.type)
         expect(response.body.amount).toBe(String(transaction.amount))
     })
+
+    it('GET /api/transactions?userId should return 200 when fatching transactions successfully', async () => {
+        const { body: createdUser } = await supertest(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const { body: createdTransaction } = await supertest(app)
+            .post('/api/transactions')
+            .send({
+                ...transaction,
+                user_id: createdUser.id,
+                id: undefined,
+            })
+
+        const response = await supertest(app).get(
+            `/api/transactions?userId=${createdUser.id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body[0].id).toEqual(createdTransaction.id)
+    })
 })
