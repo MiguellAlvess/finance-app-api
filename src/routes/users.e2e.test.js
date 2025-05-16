e948798216f72a05b3b06ebf9a323e57bedfa1b3
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-16 20:38:10 -0300

    test: add login user route

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -189,4 +189,22 @@ describe('User Routes E2E Tests', () => {
 
         expect(response.status).toBe(400)
     })
+
+    it('POST /api/users/login shhould return 200 and tokens when user crendentials are valid', async () => {
+        const { body: createdUser } = await supertest(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const response = await supertest(app).post('/api/users/login').send({
+            email: createdUser.email,
+            password: user.password,
+        })
+
+        expect(response.status).toBe(200)
+        expect(response.body.tokens.acessToken).toBeDefined()
+        expect(response.body.tokens.refreshToken).toBeDefined()
+    })
 })
