b350df91b6b47547cab390cc92885b41c8962f2f
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-17 08:54:06 -0300

    feat: add authentication middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+
+export const auth = (req, res, next) => {
+    try {
+        const acessToken = req.headers?.authorization?.split('Bearer ')[1]
+        if (!acessToken) {
+            return res.status(401).send({ message: 'Unauthorized' })
+        }
+
+        const decodedToken = jwt.verify(
+            acessToken,
+            process.env.JWT_ACESS_TOKEN_SECRET,
+        )
+
+        if (!decodedToken) {
+            return res.status(401).send({ message: 'Unauthorized' })
+        }
+
+        next()
+    } catch (error) {
+        console.error(error)
+        return res.status(401).send({ message: 'Unauthorized' })
+    }
+}
