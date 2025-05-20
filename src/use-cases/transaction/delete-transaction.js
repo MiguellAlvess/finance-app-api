4956ffae1c0042a7b622433dae0d9053f352d512
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-20 17:22:31 -0300

    feat: ensure user can only delete his transactions

--- src/use-cases/transaction/delete-transaction.js
+++ src/use-cases/transaction/delete-transaction.js
@@ -1,12 +1,25 @@
+import { ForbiddenError, TransactionNotFoundError } from '../../errors/index.js'
+
 export class DeleteTransactionUseCase {
-    constructor(deleteTransactionRepository) {
+    constructor(deleteTransactionRepository, getTransactionRepository) {
         this.deleteTransactionRepository = deleteTransactionRepository
+        this.getTransactionRepository = getTransactionRepository
     }
 
-    async execute(transactionId) {
+    async execute(transactionId, userId) {
         const transaction =
+            await this.getTransactionRepository.execute(transactionId)
+
+        if (!transaction) {
+            throw new TransactionNotFoundError(transactionId)
+        }
+
+        if (transaction.user_id !== userId) {
+            throw new ForbiddenError()
+        }
+        const deletedTransaction =
             await this.deleteTransactionRepository.execute(transactionId)
 
-        return transaction
+        return deletedTransaction
     }
 }
